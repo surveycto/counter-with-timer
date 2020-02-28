02849ffaf52f820e429cbc287bd7e83a74884474
Max S. Haberman <max@maxshaberman.com>
2020-02-27 20:02:56 -0500

    Completed adding reset buttons

--- source/script.js
+++ source/script.js
@@ -1,7 +1,23 @@
+/*fieldProperties = {
+    'PARAMETERS': [
+        {
+            'key': 'A',
+            'value': 2000
+        },
+        {
+            'key': 'B',
+            'value': 'ds'
+        }
+    ],
+    'CURRENT_ANSWER': null
+}*/
+
 var timerDisp = document.querySelector('#timer');
 var changer = document.querySelector('#buttonsst');
 var ssButton = document.querySelector('#startstop');
 var countDisp = document.querySelector('#count');
+var restartButtons = document.querySelector('#restartbuttons');
+var restartConfirm = document.querySelector('#restartconfirm');
 
 var parameters = fieldProperties.PARAMETERS
 var numParam = parameters.length
@@ -9,32 +25,34 @@ var numParam = parameters.length
 var timeStart;
 var unit;
 var round;
-if(numParam == 0){
+if (numParam == 0) {
     timeStart = 5000
     unit = ' ms';
 }
-else{
-    timeStart =  parameters[0].value; //Time limit on each field in milliseconds
-    if(numParam >= 2){
-        unit = ' ' + fieldProperties.PARAMETERS[1];
+else {
+    timeStart = parameters[0].value; //Time limit on each field in milliseconds
+    if (numParam >= 2) {
+        unit = ' ' + fieldProperties.PARAMETERS[1].value;
 
-        if(unit == ' cs'){
+        if (unit == ' cs') {
             round = 10;
         }
-        else if(unit == ' ds'){
+        else if (unit == ' ds') {
             round = 100;
         }
-        else if(unit == ' s'){
+        else if (unit == ' s') {
             round = 1000;
         }
-        else{
+        else {
             unit = ' ms';
             round = 1;
         }
     }
+    else {
+        unit = ' ms';
+        round = 1;
+    }
 }
-
-
 
 var complete = false;
 var timeLeft = timeStart; //Starts this way for the display.
@@ -45,7 +63,7 @@ var timerRunning = false;
 
 var startTime = 0; //This will get an actual value when the timer starts in startStopTimer();
 
-if(fieldProperties.CURRENT_ANSWER != null){
+if (fieldProperties.CURRENT_ANSWER != null) {
     timeLeft = 0;
     timerRunning = false;
     counter = fieldProperties.CURRENT_ANSWER;
@@ -56,48 +74,79 @@ if(fieldProperties.CURRENT_ANSWER != null){
 setInterval(timer, 1);
 
 function timer() {
-    if(timerRunning){
+    if (timerRunning) {
         timePassed = Date.now() - startTime;
         timeLeft = timeStart - timePassed;
     }
 
-    if(timeLeft <= 0){
+    if (timeLeft <= 0) {
         timeLeft = 0;
         timerRunning = false;
         changer.classList.add('buttonstop');
         changer.innerHTML = "Stop!";
+        restartButtons.style.display = '';
         setAnswer(counter); //Final answer is the value of the counter
     }
-    timerDisp.innerHTML = String(timeLeft/round) + unit;
+    timerDisp.innerHTML = String(Math.ceil(timeLeft / round)) + unit;
 }
 
 function startStopTimer() {
-    if(timerRunning){
+    if (timerRunning) {
+        restartButtons.style.display = '';
         timerRunning = false;
-        ssButton.innerHTML = "Start"
+        ssButton.innerHTML = "Start";
     }
-    else{
+    else {
+        restartButtons.style.display = 'none';
+        restartConfirm.innerHTML = '';
         startTime = Date.now() - timePassed;
         timerRunning = true;
         ssButton.innerHTML = "Stop";
     }
 }
 
-function countup(){
+function countup() {
     counter++;
     countDisp.innerHTML = counter;
 }
 
-function countdown(){
+function countdown() {
     counter--;
-    if(counter < 0){
+    if (counter < 0) {
         counter = 0;
     }
     countDisp.innerHTML = counter;
 }
 
+function restartconf(restarter) {
+    let warningMessage = "Are sure you would like to restart the " + restarter + "?";
+
+
+
+    warningMessage += '<br><button id="yes" class="whitebutton">Yes</button><button id="no" class="bluebutton">No</button>'
+    
+    restartConfirm.innerHTML = warningMessage;
+
+    document.querySelector('#yes').addEventListener('click', function () {
+        if(restarter == 'timer'){
+            timerDisp.innerHTML = timeLeft = timeStart;
+            timePassed = 0;
+        }
+        else if(restarter == 'counter'){
+            countDisp.innerHTML = counter = 0;
+        }
+        restartConfirm.innerHTML = null;
+    });
+
+    document.querySelector('#no').addEventListener('click', function () {
+        restartConfirm.innerHTML = null;
+    });
+}
+
+
+
 function clearAnswer() {
-    if(timerRunning){
+    if (timerRunning) {
         startStopTimer();
     }
     setAnswer(null);
