758f5ba66128d59cbb9e1ddcb7650beebf75efb8
Max S. Haberman <max@maxshaberman.com>
2020-02-27 19:10:57 -0500

    Began updated JS to use parameters

--- source/script.js
+++ source/script.js
@@ -1,9 +1,40 @@
-var timeStart = 5000; //Time limit on each field in milliseconds
-
 var timerDisp = document.querySelector('#timer');
 var changer = document.querySelector('#buttonsst');
 var ssButton = document.querySelector('#startstop');
 var countDisp = document.querySelector('#count');
+
+var parameters = fieldProperties.PARAMETERS
+var numParam = parameters.length
+
+var timeStart;
+var unit;
+var round;
+if(numParam == 0){
+    timeStart = 5000
+    unit = ' ms';
+}
+else{
+    timeStart =  parameters[0].value; //Time limit on each field in milliseconds
+    if(numParam >= 2){
+        unit = ' ' + fieldProperties.PARAMETERS[1];
+
+        if(unit == ' cs'){
+            round = 10;
+        }
+        else if(unit == ' ds'){
+            round = 100;
+        }
+        else if(unit == ' s'){
+            round = 1000;
+        }
+        else{
+            unit = ' ms';
+            round = 1;
+        }
+    }
+}
+
+
 
 var complete = false;
 var timeLeft = timeStart; //Starts this way for the display.
@@ -37,7 +68,7 @@ function timer() {
         changer.innerHTML = "Stop!";
         setAnswer(counter); //Final answer is the value of the counter
     }
-    timerDisp.innerHTML = timeLeft + " ms";
+    timerDisp.innerHTML = String(timeLeft/round) + unit;
 }
 
 function startStopTimer() {
